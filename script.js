f9a0b4a9ef0e2599b105d7126a0156be0d4a5beb
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2021-11-16 21:59:07 -0500

    x

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function start(){
     preloadImage('images/roof.jpg');
-    slideShow();
 }
 
 function showDiv() {
@@ -12,20 +11,3 @@ function showDiv() {
      img.src=url;
  }
 
- function slideShow(){
-
-    var indexValue = 0;
-            function slideShow(){
-              setTimeout(slideShow, 4000);
-              var x;
-              const img = document.querySelectorAll("img");
-              for(x = 0; x < img.length; x++){
-                img[x].style.display = "none";
-              }
-              indexValue++;
-              if(indexValue > img.length){indexValue = 1}
-              img[indexValue -1].style.display = "block";
-            }
-            slideShow();
-
- }
