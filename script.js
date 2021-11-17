6c344e067b0a58db02fee402141151ae7371797b
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2021-11-16 19:43:37 -0500

    x

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function showDiv() {
      var img=new Image();
      img.src=url;
  }
+
+ function slideShow(){
+     var i = 0;
+     var images = [];
+     var time = 3000;
+
+     //images
+     images[0] = 'images/wayneeidson.jpg';
+     images[1] = 'images/image2.jpg'
+
+     document.slide.src = images[i];
+
+     if(i < images.length - 1){
+         i++;
+     } else {
+         i = 0;
+     }
+
+     setTimeout( slideShow(), time);
+
+ }
