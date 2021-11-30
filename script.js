2ce35fe19ed11f3313c25c813240d3310a6e9b97
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2021-11-30 17:33:55 -0500

    thank you message

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function showDiv() {
 function showDiv2() {
     document.getElementById('contact').style.display = "block";
  }
+
+ function thankyouMessage(){
+    let x = document.forms["requestForm"]["name"].value;
+    let y = document.forms["requestForm"]["email"].value;
+    let z = document.forms["requestForm"]["address"].value;
+
+    if (x != "" && y != "" && z != "") {
+        alert("Thank you, we will be in touch shortly!");
+        return false;
+      }
+
+ }
