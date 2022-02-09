34b465589623a74783d3688e6366e381a5b56e04
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2022-02-09 18:53:45 -0500

    x

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,8 @@
 module.exports = async function (context, req) {
     //const { Connection, Request } = require("tedious");
-
+    let x = 1;
+    let y = 2;
     context.res.json({
-        text: "Hello from the API 4"
+        text: "Hello from the API 4" + (x+y)
     });
 };
