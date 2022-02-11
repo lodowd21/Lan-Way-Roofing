82a2ac9724b45b164c86427b5b519d977f7127d6
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2022-02-10 19:28:50 -0500

    x

--- api/message/index.js
+++ api/message/index.js
@@ -21,6 +21,24 @@ module.exports = async function (context, req) {
          }
      }
 */
+    console.log("hi getWeather2")
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=london&appid=f4c546f898560a59b0cd737648f02285");
+    //xhr.setRequestHeader('Access-Control-Allow-Origin','https://openweathermap.org/api');
+    xhr.send();
+    xhr.onload = () => {
+        console.log(xhr)
+        if (xhr.status === 200) {
+            console.log(JSON.parse(xhr.response));
+            //alert(`The weather api worked ${xhr.status}  ${xhr.statusText}`);
+        } else {
+            console.log(`error ${xhr.status} ${xhr.statusText}`)
+            //alert(`The weather api failed ${xhr.status}  ${xhr.statusText}`);
+
+        }
+    }
+
 
     let x = 1;
     let y = 2;
@@ -28,7 +46,7 @@ module.exports = async function (context, req) {
     //getWeather2();
     console.log("hi getWeather2")
     context.res.json({
-        text: "Hello from the API 6 " + (x+y)
+        text: "Hello from the API 7 " + (x + y)
     });
 
 };
