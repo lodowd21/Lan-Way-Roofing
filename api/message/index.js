60c76c06f83d27aa7f6dcd57a6c9d211081eb31c
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2022-02-10 19:16:58 -0500

    x

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,25 @@
 module.exports = async function (context, req) {
     //const { Connection, Request } = require("tedious");
+    function getWeather2(){
+        console.log("hi getWeather2")
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+        xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=london&appid=f4c546f898560a59b0cd737648f02285");
+        //xhr.setRequestHeader('Access-Control-Allow-Origin','https://openweathermap.org/api');
+        xhr.send();
+        xhr.onload = () => {
+            console.log(xhr)
+            if(xhr.status === 200){
+                console.log(JSON.parse(xhr.response));
+                //alert(`The weather api worked ${xhr.status}  ${xhr.statusText}`);
+                    } else {
+                console.log(`error ${xhr.status} ${xhr.statusText}`)
+                //alert(`The weather api failed ${xhr.status}  ${xhr.statusText}`);
+    
+            }
+         }
+     }
+
     let x = 1;
     let y = 2;
     console.log("in app/message 1 ")
@@ -8,24 +28,5 @@ module.exports = async function (context, req) {
     context.res.json({
         text: "Hello from the API 5 " + (x+y)
     });
+
 };
-
-function getWeather2(){
-    console.log("hi getWeather2")
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = false;
-    xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=london&appid=f4c546f898560a59b0cd737648f02285");
-    //xhr.setRequestHeader('Access-Control-Allow-Origin','https://openweathermap.org/api');
-    xhr.send();
-    xhr.onload = () => {
-        console.log(xhr)
-        if(xhr.status === 200){
-            console.log(JSON.parse(xhr.response));
-            //alert(`The weather api worked ${xhr.status}  ${xhr.statusText}`);
-                } else {
-            console.log(`error ${xhr.status} ${xhr.statusText}`)
-            //alert(`The weather api failed ${xhr.status}  ${xhr.statusText}`);
-
-        }
-     }
- }
