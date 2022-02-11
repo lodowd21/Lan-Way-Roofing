2f68f2bc8b319783dc9a6ac7cc994151df629fa9
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2022-02-10 19:53:18 -0500

    x

--- api/message/index.js
+++ api/message/index.js
@@ -26,11 +26,13 @@ module.exports = async function (context, req) {
     let y = 2;
     console.log("in app/message 1")
     //getWeather2();
-    var xhr = new XMLHttpRequest();
+    //var xhr = new XM;
+    const url = new URL("https://www.geeksforgeeks.org");   // Using toString() method 
+    console.log(url.toString());
 
     console.log("hi getWeather2")
     context.res.json({
-        text: "Hello from the API 7 " + (x+y)
+        text: "Hello from the API 8 " + (x+y)
     });
 
 };
