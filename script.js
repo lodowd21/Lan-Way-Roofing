16aceea77f59516787163fd5f6df76f12ff4c2d3
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2022-03-06 22:21:22 -0500

    email

--- script.js
+++ script.js
@@ -16,6 +16,36 @@ function showDiv2() {
     document.getElementById('contact').style.display = "block";
 }
 
+function sendEmail(){
+    let name = document.forms["requestForm"]["name"].value;
+    let email = document.forms["requestForm"]["email"].value;
+    let address = document.forms["requestForm"]["address"].value;
+    let city = document.forms["requestForm"]["city"].value;
+    let state = document.forms["requestForm"]["state"].value;
+    let zip = document.forms["requestForm"]["zip"].value;
+    let phoneNum = document.forms["requestForm"]["phone_num"].value;
+    let message = document.forms["requestForm"]["message"].value;
+
+
+    var xhr = new XMLHttpRequest();
+    var formData = JSON.stringify({"name": name, "email": email, "address": address, "city": city, "state": state, "zip": zip, "phone_num": phoneNum, "message", message});
+    xhr.withCredentials = false;
+    xhr.open("POST", "https://lanway-logicapp1.azurewebsites.net:443/api/lanway-la-quote-email/triggers/manual/invoke?api-version=2020-05-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=SHSUDdbpZdTXJxOWuhvB4CFWFvGW2cTKm6l15Nw5ZPg");
+    //xhr.setRequestHeader('Access-Control-Allow-Origin','https://openweathermap.org/api');
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(formData);
+    xhr.onload = () => {
+        console.log(xhr)
+        if (xhr.status === 200) {
+            //console.log(JSON.parse(xhr.response));
+            alert(`Quote requested successfully. ${xhr.status}  ${xhr.statusText}`);
+        }
+        else {
+            console.log(`error ${xhr.status} ${xhr.statusText}`)
+            alert(`Failed to process request. Try again. ${xhr.status}  ${xhr.statusText} ld: ${loginData} x: ${x}  y: ${y}`);
+        }
+    }  
+}
 
 function thankyouMessage() {
     let x = document.forms["requestForm"]["name"].value;
