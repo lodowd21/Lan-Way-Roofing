4a26c73d17700556dc9f5a51d0692b2bfb4005d5
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2022-03-14 12:20:32 -0400

    x

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ function sendEmail(){
         console.log(xhr.status)
         if (xhr.status === 200) {
             //console.log(JSON.parse(xhr.response));
+            console.log(`hi`)
             alert(`Quote requested successfully! ${xhr.status}  ${xhr.statusText}`);
         }
         else {
